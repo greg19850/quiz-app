0ace82b75998382776f72720ddff9605a0ecc9fa
Greg <greg19850@gmail.com>
2022-12-22 19:35:01 +0000

    Created basic html structure

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is listening at http://localost:3000/ Let\'s play a game!');
+});
