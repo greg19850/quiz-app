ef022b7d5f9029dd4870d6813de76c0815dca502
Greg <greg19850@gmail.com>
2022-12-26 16:35:07 +0000

    Added functions for all lifelines

--- public/script.js
+++ public/script.js
@@ -2,6 +2,17 @@ const question = document.getElementById('question');
 const goodAnswersNumber = document.getElementById('good-answers')
 const gameBoard = document.getElementById('game-board');
 const h2 = document.querySelector('h2');
+const hintDisplay = document.getElementById('hint');
+const buttons = document.querySelectorAll('.answer-btn');
+
+for (const button of buttons) {
+
+  button.addEventListener('click', (event) => {
+    const answerIndex = event.target.dataset.key;
+
+    sendAnswer(answerIndex)
+  })
+};
 
 
 function showNextQuestion() {
@@ -24,9 +35,11 @@ function showNextQuestion() {
       };
 
       question.innerText = data.question;
+      hintDisplay.innerText = '';
 
       data.answers.forEach((answer, index) => {
-        const answerBtn = document.getElementById(`answer${index + 1}`)
+        const answerBtn = document.getElementById(`answer${index + 1}`);
+        answerBtn.style.display = 'inline-block'
         answerBtn.innerText = answer
       })
     })
@@ -35,7 +48,7 @@ function showNextQuestion() {
 showNextQuestion();
 
 function sendAnswer(ansIndex) {
-  fetch(`./answer/${ansIndex}`, {
+  fetch(`/answer/${ansIndex}`, {
     method: 'POST',
   })
     .then(resp => resp.json())
@@ -45,13 +58,66 @@ function sendAnswer(ansIndex) {
     })
 };
 
-const buttons = document.querySelectorAll('button');
 
-for (const button of buttons) {
-
-  button.addEventListener('click', (event) => {
-    const answerIndex = event.target.dataset.key;
-
-    sendAnswer(answerIndex)
+function phoneAFriend() {
+  fetch('/help/friend', {
+    method: 'GET',
   })
+    .then(resp => resp.json())
+    .then(data => {
+      hintDisplay.textContent = data.text
+    })
+};
+
+document.getElementById('phoneAFriend').addEventListener('click', phoneAFriend);
+
+function handleFiftyFiftyLifeline(data) {
+
+  if (typeof data.text === 'string') {
+    hintDisplay.textContent = data.text
+  } else {
+    for (const button of buttons) {
+      if (data.answersToRemove.indexOf(button.innerText) > -1) {
+        button.style.display = 'none'
+      }
+    }
+  }
 }
+
+function fiftyFifty() {
+  fetch('/help/fifty', {
+    method: 'GET',
+  })
+    .then(resp => resp.json())
+    .then(data => {
+      hintDisplay.innerText = '';
+      handleFiftyFiftyLifeline(data)
+    })
+};
+
+document.getElementById('fiftyFifty').addEventListener('click', fiftyFifty);
+
+function handleAudienceAnswer(data) {
+
+  if (typeof data.text === 'string') {
+    hintDisplay.innerText = data.text;
+  } else {
+    data.chart.forEach((percent, index) => {
+
+      buttons[index].innerText = `${buttons[index].innerText}: ${percent}%`;
+    });
+  }
+}
+
+function askTheAudience() {
+  fetch('/help/audience', {
+    method: 'GET',
+  })
+    .then(resp => resp.json())
+    .then(data => {
+      hintDisplay.innerText = '';
+      handleAudienceAnswer(data);
+    });
+}
+
+document.getElementById('askTheAudience').addEventListener('click', askTheAudience);
--- routes/game.js
+++ routes/game.js
@@ -83,8 +83,81 @@ function gameRoutes(app) {
       goodAnswers,
     })
   })
-};
 
+
+  app.get('/help/friend', (req, res) => {
+
+    if (phoneAFriendUsed) {
+      return res.json({
+        text: 'You have already used this lifeline'
+      })
+    }
+
+    phoneAFriendUsed = true;
+
+    const friendKnowsAnswer = Math.random() < 0.5;
+
+    const question = questions[goodAnswers];
+
+    res.json({
+      text: friendKnowsAnswer ? `I think answer is ${question.answers[question.correctAnswer]}` : 'Sorry, but I don\'t know the answer'
+    })
+  });
+
+  app.get('/help/fifty', (req, res) => {
+
+    if (fiftyFiftyUsed) {
+      return res.json({
+        text: 'You have already used this lifeline'
+      })
+    }
+
+    fiftyFiftyUsed = true;
+
+    const question = questions[goodAnswers];
+
+    const answersCopy = question.answers.filter((answer, index) => (
+      index !== question.correctAnswer
+    ));
+
+    answersCopy.splice(Math.floor(Math.random() * answersCopy.length), 1);
+
+    res.json({
+      answersToRemove: answersCopy,
+    })
+  });
+
+
+  app.get('/help/audience', (req, res) => {
+
+    if (askTheAudienceUsed) {
+      return res.json({
+        text: 'You have already used this lifeline',
+      });
+    }
+
+    askTheAudienceUsed = true;
+
+    const chart = [10, 20, 30, 40];
+
+    for (let i = chart.length - 1; i > 0; i--) {
+      const change = Math.floor(Math.random() * 20 - 10);
+
+      chart[i] += change;
+      chart[i - 1] -= change;
+    }
+
+    const question = questions[goodAnswers];
+    const { correctAnswer } = question;
+
+    [chart[3], chart[correctAnswer]] = [chart[correctAnswer], chart[3]];
+
+    res.json({
+      chart,
+    });
+
+  });
+};
 
 
 module.exports = gameRoutes;
